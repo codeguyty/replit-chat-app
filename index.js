b99409cd2acb298743801b26f60cf7533ef92322
Tyler Moyer <62945666+codeguyty@users.noreply.github.com>
2023-03-07 00:20:35 +0000

    Built the chat client using Socket.io client and Readline Module.

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-// add a reference to expressjs and create a new express app
-const express = require("express");
-const app = express();
+// Adding Socket.io-client and readline module
+const io = require("socket.io-client");
+const readline = require("readline");
 
-// Add a reference to HTTP, and create a new server with the express framework I created above
-const http = require("http");
-const server = http.Server(app);
+// Create a new connection to the chat server
+var socket = io("https://repl-chat-app.codeguyty.repl.co");
 
-// extend server to support websockets for make chat responsive in realtime
-const sockets = require("socket.io");
-io = sockets(server);
+// Creating a console interface using readline module
+const chat_interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// code to handle new connection events and incoming messages from clients
-io.on('connection', function(connection) {
-  connection.on('message', function (data) {
-    console.log('new message: ' + data);
-    io.emit("broadcast", data);
+// To remember the user's chat handle and message they send
+var chat_handle = "";
+var message_to_send = "";
+
+// When we sucessfully connect to the chat server / When the server broadcasts messages to us
+socket.on("connect", function(){
+  get_chat_handle();
+  socket.on("broadcast", display_message);
+});
+
+// Gets the user's name, so we can introduce and prepend each message with their name
+function get_chat_handle() {
+  chat_interface.question(`Hello! What's your handle? `,
+    function(answer) {
+      chat_handle = answer;
+      socket.emit("message", chat_handle + ' has entered the chat');
+      chat();
+    }
+  );
+}
+
+// Waits for a new message to send
+function chat() {
+  chat_interface.question(chat_handle + ": ", 
+    function(message) {
+      message_to_send = chat_handle + ": ", message;
+      socket.emit("message", message_to_send);
+      chat();
   });
-});
+}
 
-//code to start the server
-server.listen(3000, function() {
-  console.log("listening on port 3000");
-});
+// Handles an incoming message, and checks to see that it is not the one we sent.
+// Shows it on the console if it is from another user.
+function display_message(message) {
+  if (message_to_send != message) {
+    console.log("\n" + message);
+    chat();
+  }
+}
